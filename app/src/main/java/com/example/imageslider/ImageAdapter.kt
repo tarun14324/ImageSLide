938c3411bfa194c9e23fc8a335280ea225179610
tharun.lanka <tharun.lanka@narayanagroup.com>
2022-11-02 12:56:29 +0530

    message

---
+++ app/src/main/java/com/example/imageslider/ImageAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.imageslider
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.example.imageslider.databinding.ItemImageSlideViewBinding
+
+class ImageAdapter(val imageList: ArrayList<Int>, val viewPager: ViewPager2) :
+    RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+    private lateinit var binding: ItemImageSlideViewBinding
+
+    class ImageViewHolder(view: ItemImageSlideViewBinding) : RecyclerView.ViewHolder(view.root) {
+        val image = view.image
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.item_image_slide_view,
+            parent,
+            false
+        )
+        return ImageViewHolder(binding)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.image.setImageResource(imageList[position])
+        if (position == imageList.size - 1) {
+            viewPager.post {
+                imageList.addAll(imageList)
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+}
---
+++ app/src/main/java/com/example/imageslider/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.imageslider
+
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import com.example.imageslider.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: ImageAdapter
+    private lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        adapter = ImageAdapter(
+            arrayListOf(
+                R.drawable.one,
+                R.drawable.two,
+                R.drawable.three,
+                R.drawable.four,
+                R.drawable.five,
+                R.drawable.six,
+                R.drawable.seven,
+                R.drawable.eight,
+            ), binding.viewPager2
+        )
+        binding.viewPager2.offscreenPageLimit = 3
+        binding.viewPager2.adapter = adapter
+        setUpTransformer()
+        handler = Handler(Looper.myLooper()!!)
+        binding.viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable, 2000)
+            }
+
+        })
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler.postDelayed(runnable, 2000)
+    }
+
+    private val runnable = Runnable {
+        binding.viewPager2.currentItem = binding.viewPager2.currentItem + 1
+    }
+
+    private fun setUpTransformer() {
+        val transformer = CompositePageTransformer()
+        transformer.addTransformer(MarginPageTransformer(40))
+        transformer.addTransformer { page, position ->
+            val r = 1 - kotlin.math.abs(position)
+            page.scaleY = 0.85f + r * 0.14f
+        }
+
+        binding.viewPager2.setPageTransformer(transformer)
+    }
+}
+
+
